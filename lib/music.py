2f9cb577b2f32d511707650749fe91185134d00a
anonymous <thefifthman55@gmail.com>
2019-01-19 21:01:24 +0800

    add song

--- lib/music.py
+++ lib/music.py
@@ -4,7 +4,7 @@
 @Version: 
 @Author: 
 @Date: 2019-01-18 22:46:11
-@LastEditTime: 2019-01-19 20:43:58
+@LastEditTime: 2019-01-19 20:57:41
 '''
 import requests
 from urllib.parse import quote
@@ -21,7 +21,7 @@ def search_music(pages,music_name,type_of_search):
         payload = "input=" + quote(music_name) + "&filter=name&type="+ type_of_search +"&page=" + str(i)
         Q.put(payload)
 
-def worker(music_name,headers2):
+def worker(music_name,headers2,song=False):
     while not Q.empty():
         payload = Q.get()
         headers = {
@@ -43,7 +43,6 @@ def worker(music_name,headers2):
                 if not os.path.exists(path):
                     os.makedirs(path)
                 
-                
                 if res.status_code == 200:
                     succeed_output(" [-] title: {} - {}".format(data['title'],res.status_code))
                     with open(os.path.join(path,data['title'] + ".mp3"),'wb')as f:
@@ -51,10 +50,12 @@ def worker(music_name,headers2):
                 else:
                     failed_output(" [-] title: {} - {}".format(data['title'],res.status_code))
 
-    
+                if song:
+                    return 
 
 def music_run():
     music_name = input(" [-] 输入要搜索的音乐：")
+    song = input(" [-] 搜索歌曲 Y/N: ")
     pages = int(input(" [-] 搜索几页数据："))
     type_of_search = input(""" [-] 请输入什么类型的搜索: \n [1] QQ音乐 [2] 酷狗音乐 [3]网易音乐 [4] 酷我音乐\n [-] 输入: """)
     if type_of_search == '1':
@@ -106,12 +107,15 @@ def music_run():
         }
 
     search_music(pages,music_name,type_of_search)
-    ts = [ threading.Thread(target=worker,args=(music_name,headers2)) for i in range(30)]
-    for t in ts:
-        t.start()
+    if song == 'Y' or song == 'y':
+        worker(music_name,headers2,song=True)
+    else:
+        ts = [ threading.Thread(target=worker,args=(music_name,headers2)) for i in range(30)]
+        for t in ts:
+            t.start()
 
-    for t in ts:
-        t.join()
+        for t in ts:
+            t.join()
         
     succeed_output(" [-] Completed")
     
